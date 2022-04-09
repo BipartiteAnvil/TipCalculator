6613c94727551cee9befb4b7dc03bf707d2392f9
BipartiteAnvil <tahsin029@gmail.com>
2022-04-09 14:13:36 +0600

    Day 2 for Tip Calculator Completed without Extensions

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tipcalculator
 
+import android.animation.ArgbEvaluator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -7,6 +8,7 @@ import android.text.TextWatcher
 import android.util.Log
 import android.widget.SeekBar
 import androidx.constraintlayout.widget.StateSet.TAG
+import androidx.core.content.ContextCompat
 import com.example.tipcalculator.databinding.ActivityMainBinding
 
 private const val TAG = "MainActivity"
@@ -23,11 +25,14 @@ class MainActivity : AppCompatActivity() {
 
         binding.sbTip.progress = INITIAL_TIP_PERCENT
         binding.tvTipParcent.text = "${INITIAL_TIP_PERCENT}%"
+        updateTipDescription(INITIAL_TIP_PERCENT)
+
         binding.sbTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, P1: Int, P2: Boolean) {
                 Log.i(TAG, "On progress Changed $P1")
                 binding.tvTipParcent.text = "$P1"
                 computeTipAndTotal()
+                updateTipDescription(P1)
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {}
@@ -45,6 +50,29 @@ class MainActivity : AppCompatActivity() {
                 computeTipAndTotal()
                 }
             })
+    }
+
+    private fun updateTipDescription(tipPercent: Int) {
+
+        val tipDescriprtion = when(tipPercent){
+            in 0..9 -> "Poor !"
+            in 10..14 -> "Acceptable"
+            in 15..19 -> "Good"
+            in 20..24 -> "Great !"
+            else -> "Amazing !!!"
+
+        }
+        binding.tvTipDescription.text = tipDescriprtion
+        //below changing color using interpolation concept
+        // best tip -> green ; worst tip -> red
+        // tip in between will have color in between
+        // android has ARGB-Evaluator to do this
+        val color = ArgbEvaluator().evaluate(
+            tipPercent.toFloat() / binding.sbTip.max,
+            ContextCompat.getColor(this, R.color.colorWorstTip),
+            ContextCompat.getColor(this, R.color.colorBestTip)
+        )   as Int
+        binding.tvTipDescription.setTextColor(color)
     }
 
     private fun computeTipAndTotal() {
